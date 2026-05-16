9d2e7d2ef98d815568e544f6091f47ec79b9ab71
hashimmalik59 <hm3630450@gmail.com>
2026-05-17 00:55:42 +0500

    Changing text of sign up form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -86,7 +86,7 @@ const Signup = () => {
               <span>Registering...</span>
             </>
           ) : (
-            "Register Gari Account"
+            "Register"
           )}
         </motion.button>
       </form>
